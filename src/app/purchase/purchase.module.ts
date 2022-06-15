daa85869dd24dfe2c2fd3a50c9104965b92f3fe5
jsriveraa1 <js.riveraa1@uniandes.edu.co>
2022-06-15 09:29:51 -0500

    * Primer commit

---
+++ src/app/purchase/purchase.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PurchaseOrderComponent } from './purchase-order/purchase-order.component';
+
+
+
+@NgModule({
+  declarations: [
+    PurchaseOrderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PurchaseModule { }
